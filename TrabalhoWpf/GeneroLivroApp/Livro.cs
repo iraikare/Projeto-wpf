c5c9bb0551c23439606bb5995620e41e519399cc
Iraikare Rodrigues de Santana Alves Vieira <iraikare.rodrigues@academico.ifrn.edu.br>
2022-12-16 10:02:12 -0300

    primeiro commit

---
+++ TrabalhoWpf/GeneroLivroApp/Livro.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeneroLivroApp
+{
+    class Livro
+    {
+        private string nome, autores;
+        private double preco;
+        public Livro(string nome, string autores, double preco)
+        {
+            if (nome != null) this.nome = nome;
+            if (autores != null) this.autores = autores;
+            if (preco > 0) this.preco = preco;
+        }
+        public double GetPreco()
+        {
+            return preco;
+        }
+        public override string ToString()
+        {
+            return $"Livro: {nome} - autor(es): {autores} - preco: {preco}";
+        }
+    }
+}
